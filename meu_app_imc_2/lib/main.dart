f08607448865e5221abfa569913bffed142f5f3d
Samuel Silva <samuel7silva@outlook.com>
2023-09-26 12:16:16 -0300

    configura o hive no app

--- meu_app_imc_2/lib/main.dart
+++ meu_app_imc_2/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:meu_app_imc_2/models/imc_hive_model.dart';
 import 'package:meu_app_imc_2/my_app.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentsDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentsDirectory.path);
+  Hive.registerAdapter(IMCHiveModelAdapter());
   runApp(const MyApp());
 }
