0f6642875146a532850a9f870e65a57cbf16c9f8
Samuel Silva <samuel7silva@outlook.com>
2023-10-03 12:14:40 -0300

    cria funçao para listar os ceps do back4app

--- viacep/lib/screens/ceps.dart
+++ viacep/lib/screens/ceps.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:viacep/models/cadastro_cep.dart';
+import 'package:viacep/repositories/cep_back4app_repository.dart';
 import 'package:viacep/screens/cadastrar_cep.dart';
 import 'package:viacep/screens/cep_details.dart';
+import 'package:viacep/screens/editar_cep.dart';
 
 class CepsCadastrados extends StatefulWidget {
   const CepsCadastrados({super.key});
@@ -10,6 +13,25 @@ class CepsCadastrados extends StatefulWidget {
 }
 
 class _CepsCadastradosState extends State<CepsCadastrados> {
+  CepBack4AppRepository cepBack4AppRepository = CepBack4AppRepository();
+  var _ceps = CadastroCepModel([]);
+  var loading = false;
+  @override
+  void initState() {
+    loadCeps();
+    super.initState();
+  }
+
+  void loadCeps() async {
+    setState(() {
+      loading = true;
+    });
+    _ceps = await cepBack4AppRepository.get();
+    setState(() {
+      loading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,48 +50,60 @@ class _CepsCadastradosState extends State<CepsCadastrados> {
           }),
       body: Container(
         padding: const EdgeInsets.all(16),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext bc) => const CepView()));
-                    },
-                    child: const Row(
-                      children: [
-                        Text("35850-000"),
-                      ],
-                    )),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.delete))
-              ],
-            ),
-            const Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext bc) => const CepView()));
-                    },
-                    child: const Row(
-                      children: [
-                        Text("35850-000"),
-                      ],
-                    )),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.delete))
-              ],
-            ),
-            const Divider(),
-          ],
-        ),
+        child: loading
+            ? const Center(heightFactor: 2, child: CircularProgressIndicator())
+            : Expanded(
+                child: ListView.builder(
+                    itemCount: _ceps.results.length,
+                    itemBuilder: (BuildContext bc, int index) {
+                      var cep = _ceps.results[index];
+                      return Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (BuildContext bc) =>
+                                                CardDetailPage(
+                                                  cepDetail: cep,
+                                                )));
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Text(cep.cep),
+                                      ],
+                                    ),
+                                  )),
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.delete)),
+                                  IconButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (BuildContext bc) =>
+                                                    EditarCep(
+                                                      cepDetail: cep,
+                                                    )));
+                                      },
+                                      icon: const Icon(Icons.edit)),
+                                ],
+                              )
+                            ],
+                          ),
+                          const Divider()
+                        ],
+                      );
+                    })),
       ),
     ));
   }
