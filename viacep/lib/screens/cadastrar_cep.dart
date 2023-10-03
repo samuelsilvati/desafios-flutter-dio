5fdd988ca39b0b594942b9ebe5d575304b6212a7
Samuel Silva <samuel7silva@outlook.com>
2023-10-03 09:41:13 -0300

    termina a tela de cadastrar tarefas

--- viacep/lib/screens/cadastrar_cep.dart
+++ viacep/lib/screens/cadastrar_cep.dart
@@ -17,17 +17,84 @@ class _CadastrarCepState extends State<CadastrarCep> {
         title: const Text("Cadastrar Cep"),
       ),
       body: Container(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(8),
         child: ListView(
           children: [
-            InkWell(
-                onTap: () {},
-                child: const Row(
-                  children: [
-                    Text("35850-000"),
-                  ],
-                )),
-            const Divider(),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                maxLength: 8,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    counterText: "",
+                    border: OutlineInputBorder(),
+                    labelText: "Cep"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Logradouro"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Complemento"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Bairro"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Localidade"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: TextField(
+                maxLength: 2,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    counterText: "",
+                    border: OutlineInputBorder(),
+                    labelText: "UF"),
+                // controller: nomeUsuarioController,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                      backgroundColor: MaterialStateProperty.all(
+                        Theme.of(context).colorScheme.inversePrimary,
+                      )),
+                  onPressed: () async {},
+                  child: const Text(
+                    "Cadastrar",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
